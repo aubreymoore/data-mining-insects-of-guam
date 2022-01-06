ccf3f517fdfd8dcdb014c4c541b07f5b71c74f5f
aubreymoore <aubreymoore@guam.net>
2022-01-06 17:56:02 +0800

    validator2

---
+++ validator2/cron.md
@@ -0,0 +1,1 @@
+https://unix.stackexchange.com/questions/487448/script-in-etc-cron-hourly-never-running
---
+++ validator2/run_validator2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Desktop/data-mining-insects-of-guam/validator2/
+/home/aubrey/.local/bin/papermill validator2.ipynb output.ipynb
+
